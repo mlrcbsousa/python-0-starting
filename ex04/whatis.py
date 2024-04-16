b89101a48a940cbe210a7f7f90d211aacf9cf7c4
Manuel Sousa <mlrcbsousa@gmail.com>
2024-04-16 10:26:06 +0200

    Add ex04 whatis.py

---
+++ ex04/whatis.py
@@ -0,0 +1,26 @@
+import sys
+
+def is_num(n: int) -> bool:
+    try:
+        int(n)
+        return True
+    except ValueError:
+        return False
+
+
+if len(sys.argv) != 1:
+    arg = sys.argv[1]
+
+    try:
+        assert len(sys.argv) == 2, 'more than one argument is provided'
+        assert is_num(arg), 'argument is not an integer'
+
+    except AssertionError as error:
+        print(f"AssertionError: {error}")
+
+    else:
+        if int(arg) % 2 == 0:
+            print("I'm Even.")
+        else:
+            print("I'm Odd.")
+
